54c4e3688b22eec54def0f7f05c9212621158c5b
Tony Tran <macair@macairs-MacBook-Air.local>
2022-01-06 19:10:39 +0700

    ERC20
    ERC20 token

---
+++ ETHEToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+//.
+pragma solidity ^0.8.0;
+import "./BEP20Detailed.sol";
+import "./BEP20.sol";
+
+
+contract ETHEToken is BEP20Detailed, BEP20 {
+  constructor() BEP20Detailed("EtheKing", "ETHE", 18) {
+    uint256 totalTokens = 100000000 * 10**uint256(decimals());
+    _mint(msg.sender, totalTokens);
+  }
+
+  function burn(uint256 amount) external {
+    _burn(msg.sender, amount);
+  }
+}
